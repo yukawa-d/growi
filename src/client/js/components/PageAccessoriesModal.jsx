7a9c8a96614f0fe0b2583cb63f893019736f650e
kaori <kaori@weseek.co.jp>
2020-10-15 14:15:01 +0900

    remove comments

--- src/client/js/components/PageAccessoriesModal.jsx
+++ src/client/js/components/PageAccessoriesModal.jsx
@@ -32,45 +32,6 @@ const PageAccessoriesModal = (props) => {
   const [sliderWidth, setSliderWidth] = useState(null);
   const [sliderMarginLeft, setSliderMarginLeft] = useState(null);
 
-  // function closeModalHandler() {
-  //   if (props.onClose == null) {
-  //     return;
-  //   }
-  //   props.onClose();
-  // }
-
-  // Might make this dynamic for px, %, pt, em
-  // function getPercentage(min, max) {
-  //   return min / max * 100;
-  // }
-
-  // useEffect(() => {
-  //   if (activeTab === '') {
-  //     return;
-  //   }
-
-  //   const navTitle = document.getElementById('nav-title');
-  //   const navTabs = document.querySelectorAll('li.nav-link');
-
-  //   if (navTitle == null || navTabs == null) {
-  //     return;
-  //   }
-
-  //   let tempML = 0;
-
-  //   const styles = [].map.call(navTabs, (el) => {
-  //     const width = getPercentage(el.offsetWidth, navTitle.offsetWidth);
-  //     const marginLeft = tempML;
-  //     tempML += width;
-  //     return { width, marginLeft };
-  //   });
-
-  //   const { width, marginLeft } = styles[navTabMapping[activeTab].index];
-
-  //   setSliderWidth(width);
-  //   setSliderMarginLeft(marginLeft);
-
-  // }, [activeTab]);
 
   const navTabMapping = {
     pagelist: {
@@ -105,58 +66,12 @@ const PageAccessoriesModal = (props) => {
     },
   };
 
-  // pagelist: {
-  //   icon: <PageListIcon />,
-  //   i18n: t('page_list'),
-  //   // [TODO: show trash page list by gw4064]
-  //   tabContent: t('Trash page list'),
-  //   index: 0,
-  // },
-
 
   return (
     <React.Fragment>
       <div className="grw-trash-page">
         <CustomNavigation navTabMapping={navTabMapping} isOpen={props.isOpen} onClose={props.onClose} />
       </div>
-      {/* <Modal size="xl" isOpen={props.isOpen} toggle={closeModalHandler} className="grw-page-accessories-modal">
-        <ModalHeader className="p-0" toggle={closeModalHandler}>
-          <Nav className="nav-title" id="nav-title">
-            {Object.entries(navTabMapping).map(([key, value]) => {
-              return (
-                <NavItem key={key} type="button" className={`p-0 nav-link ${activeTab === key && 'active'}`}>
-                  <NavLink onClick={() => { switchActiveTab(key) }}>
-                    {value.icon}
-                    {t(value.i18n)}
-                  </NavLink>
-                </NavItem>
-              );
-            })}
-          </Nav>
-          <hr className="my-0 grw-nav-slide-hr border-none" style={{ width: `${sliderWidth}%`, marginLeft: `${sliderMarginLeft}%` }} />
-        </ModalHeader>
-        <ModalBody className="overflow-auto grw-modal-body-style p-0">
-          <TabContent activeTab={activeTab} className="p-5">
-            <TabPane tabId="pagelist">
-              {pageAccessoriesContainer.state.activeComponents.has('pagelist') && <PageList />}
-            </TabPane>
-            <TabPane tabId="timeline">
-              {pageAccessoriesContainer.state.activeComponents.has('timeline') && <PageTimeline /> }
-            </TabPane>
-            <TabPane tabId="pageHistory">
-              <div className="overflow-auto">
-                {pageAccessoriesContainer.state.activeComponents.has('pageHistory') && <PageHistory /> }
-              </div>
-            </TabPane>
-            <TabPane tabId="attachment">
-              {pageAccessoriesContainer.state.activeComponents.has('attachment') && <PageAttachment />}
-            </TabPane>
-            <TabPane tabId="shareLink">
-              {pageAccessoriesContainer.state.activeComponents.has('shareLink') && <ShareLink />}
-            </TabPane>
-          </TabContent>
-        </ModalBody>
-      </Modal> */}
     </React.Fragment>
   );
 };
